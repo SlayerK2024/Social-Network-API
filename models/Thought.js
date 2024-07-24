88f9b9c6ec905a5cf01bced055dca891a78079c0
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-07-24 17:32:04 -0400

    Update Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,48 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const thoughtSchema = new Schema({
-  thoughtText: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  reactions: [{ type: Schema.Types.ObjectId, ref: 'Reaction' }],
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp)
+  }
 });
 
-const Thought = mongoose.model('Thought', thoughtSchema);
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp)
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema]
+  }, {timestamps: true });
 
-module.exports = Thought;
+  thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+module.exports = mongoose.model('Thought', ThoughtSchema);
