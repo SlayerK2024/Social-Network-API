131d908d9c305559dbccf0667a323c2570df0652
Kyna P <157855730+SlayerK2024@users.noreply.github.com>
2024-07-24 17:52:31 -0400

    utils

---
+++ utils/dateUtils.js
@@ -0,0 +1,5 @@
+exports.formatDate = (date) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return new Date(date).toLocaleDateString(undefined, options);
+  };
+  
