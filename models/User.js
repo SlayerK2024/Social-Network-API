87197d542336afb97480ef728c452c553ebe1f31
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-07-24 15:34:58 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }]
+  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 });
 
 const User = mongoose.model('User', userSchema);
