ea78dcc87811c2007dffb06b9b071b488c24a77e
SlayerK2024 <157855730+SlayerK2024@users.noreply.github.com>
2024-07-24 17:33:47 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,35 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config').get(process.env.NODE_ENV)
 
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
-   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-});
+const UserSchema = new Schema({
+   username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+            },
+           thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+           }],
+           friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+           }]
+           }, {timestamps: true})
 
-const User = mongoose.model('User', userSchema);
+           userSchema.virtual('friendCount').get(function() {
+            return this.friends.length;
+          });
 
-module.exports = User;
+           module.exports = mongoose.model('User', UserSchema)
